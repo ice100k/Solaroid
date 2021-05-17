32ff6510ba3af47e9d058724fe883759ec01ff78
ice100k <ice100k@outlook.com>
2021-05-17 18:36:35 +0100

    WHY WONT IT REMOVE READONLY

--- SolaroidInstaller/Solaroid/Program.cs
+++ SolaroidInstaller/Solaroid/Program.cs
@@ -1,8 +1,10 @@
 ﻿//Solaroid
-//By ice100k and thecrisperson
+//By ice100k and thecrisperson, Lore by Literally_no1
 using System;
 using System.IO;
 using System.Net;
+using System.Security.Principal;
+using System.Security.AccessControl;
 
 namespace Solaroid {
 
@@ -16,7 +18,7 @@ namespace Solaroid {
             Console.WriteLine("Made by: ice100k, thecrisperson and literaly_no1");
             Console.WriteLine("");
             Console.WriteLine("All Solaroid files will be downloaded here:");
-            Console.WriteLine((Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles).ToString()) + "\\Solaroid");
+            Console.WriteLine((Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles).ToString()) + "\\Solaroid\\");
             Console.WriteLine("");
             Console.WriteLine("Press A to continue or press Esc to quit ");
 
@@ -30,33 +32,56 @@ namespace Solaroid {
             }
 
             Console.WriteLine("");
-            Console.WriteLine("Download Solaroid Core? (y/n) ");
+            Console.WriteLine("Download Solaroid Core? (Y/N) ");
 
             while (true) {
                 KeyInfo = Console.ReadKey();
                 if (KeyInfo.Key == ConsoleKey.Y) {
-                    string extractPath = ((Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles).ToString()) + "\\Solaroid");
+                    string extractPath = ((Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles).ToString()) + @"\Solaroid");
 
-                    System.IO.Directory.CreateDirectory(extractPath);
-                    Console.WriteLine("Created Path " + extractPath);
+                    if (Directory.Exists(extractPath)) {
+                        Console.WriteLine("");
+                        Console.WriteLine(extractPath + " already exists. Skipping step...");
+                    } else {
+                        try {
+                            Console.WriteLine("");
+                            DirectoryInfo dInfo = Directory.CreateDirectory(extractPath);
+                            Console.WriteLine("Directory {0} was created sucessfully at: ", Directory.GetCreationTime(extractPath));
+
+                            DirectorySecurity dSecurity = dInfo.GetAccessControl();
+
+                            dSecurity.AddAccessRule(new FileSystemAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), FileSystemRights.FullControl, InheritanceFlags.ObjectInherit | InheritanceFlags.ContainerInherit, PropagationFlags.NoPropagateInherit, AccessControlType.Allow));
+                            dInfo.SetAccessControl(dSecurity);
+
+                        } catch (Exception e) {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
+                    }
+
+                    DirectoryInfo dI = new DirectoryInfo(extractPath);
+                    dI.Attributes |= FileAttributes.ReadOnly;
+                    dI.Attributes -= FileAttributes.ReadOnly;
+                    dI.Refresh();
 
                     Console.WriteLine("");
                     Console.WriteLine("Downloading.... ");
 
                     using (var Client = new WebClient()) {
-
-                        Client.DownloadFile(
-
-                        new System.Uri("https://github.com/ice100k/Solaroid/raw/main/Solaroid.zip"),
-
-                        extractPath
-                    );
-
-                        System.IO.Compression.ZipFile.ExtractToDirectory(extractPath + "\\Solaroid.zip", extractPath);
-                        File.Delete(extractPath + "\\Solaroid.zip");
-
-                        Console.WriteLine("Done!");
+                        try {
+                            Client.DownloadFile(new System.Uri("https://github.com/ice100k/Solaroid/raw/main/Solaroid.zip"), extractPath);
+                        } catch (Exception e) {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
                     }
+
+                    Console.WriteLine("");
+                    Console.WriteLine("Unzipping Core...");
+
+                    System.IO.Compression.ZipFile.ExtractToDirectory(extractPath + "\\Solaroid.zip", extractPath);
+                    File.Delete(extractPath + "\\Solaroid.zip");
+
+                    Console.WriteLine("");
+                    Console.WriteLine("Sucsessfully setup Core!");
 
                     if (KeyInfo.Key == ConsoleKey.N)
                         break;
